2b143d06bc7f903d5ada295646f6a3d5144e2206
MayurSMahajan <mayur23smahajan@gmail.com>
2024-07-14 22:48:47 +0530

    return a conditional response

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,25 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+  // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
   socket.on("close", () => {
     socket.end();
   });
+
+  socket.on("data", (data) => {
+    // convert byte data into 
+    const req = data.toString();
+    // splitting the string using whitespace since a typical data will look like
+    // GET /abcdef HTTP1.1 ...
+    const strArr = req.split(" ");
+    if (strArr[1] === '/'){
+      socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+    }else{
+      socket.write(Buffer.from(`HTTP/1.1 404 NOT FOUND\r\n\r\n`));
+    }
+    socket.end();
+    
+  })
 });
 
 server.listen(4221, "localhost");
