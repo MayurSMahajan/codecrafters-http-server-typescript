3aef0eee0c4ac37b4166cadaee2523f6530ffea1
MayurSMahajan <mayur23smahajan@gmail.com>
2024-07-24 22:27:39 +0530

    handle echo request

--- app/main.ts
+++ app/main.ts
@@ -13,14 +13,32 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     // convert byte data into 
     const req = data.toString();
+    
     // splitting the string using whitespace since a typical data will look like
     // GET /abcdef HTTP1.1 ...
-    const strArr = req.split(" ");
+    const strArr = req.split(' ');
     if (strArr[1] === '/'){
       socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+    }
+    else if(strArr[1].split("/")[1] === 'echo'){
+      const content = strArr[1].split("/")[2];
+      if(content && content.length){
+        socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`));
+        socket.end();
+      }
+      socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+      
     }else{
       socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
     }
+
+    // const strArr = req.split(" ");
+    // if (strArr[1] === '/'){
+
+    //   socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+    // }else{
+    //   socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
+    // }
     socket.end();
     
   })
