5cd9b344dd72e42b78e4a52f9fd76b126b554351
MayurSMahajan <mayur23smahajan@gmail.com>
2024-07-27 14:25:21 +0530

    feat: return a file if exists

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import * as net from "net";
+import fs from "node:fs";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -10,6 +11,11 @@ const server = net.createServer((socket) => {
     socket.end();
   });
 
+  function changeResponse(response: string){
+    socket.write(Buffer.from(response));
+    socket.end();
+  }
+
   socket.on("data", (data) => {
     // convert byte data into 
     const req = data.toString();
@@ -17,36 +23,47 @@ const server = net.createServer((socket) => {
     // splitting the string using whitespace since a typical data will look like
     // GET /abcdef HTTP1.1 ...
     const strArr = req.split(' ');
+    const param = strArr[1].split("/")[1];
+    
     if (strArr[1] === '/'){
-      socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+      changeResponse(`HTTP/1.1 200 OK\r\n\r\n`);
     }
-    else if(strArr[1].split("/")[1] === 'echo'){
+    else if(param === 'echo'){
       const content = strArr[1].split("/")[2];
       if(content && content.length){
-        socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`));
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`;
+        changeResponse(res);
       }else{
-        socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+        changeResponse(`HTTP/1.1 200 OK\r\n\r\n`);
       }
     }
-    else if(strArr[1].split("/")[1] === 'user-agent'){
+    else if(param === 'user-agent'){
       const uaInfo = req.split("User-Agent:")[1]?.split(' ')[1].trim();
       if(uaInfo){
-        socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${uaInfo.length}\r\n\r\n${uaInfo}\r\n\r\n`));
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${uaInfo.length}\r\n\r\n${uaInfo}\r\n\r\n`;
+        changeResponse(res);
       }else{
-        socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+        changeResponse(`HTTP/1.1 200 OK\r\n\r\n`);
+      }
+    }
+    else if(param === 'files'){
+      const args = process.argv.slice(2);
+      const [___, absPath] = args;
+      const path = strArr[1].split("/")[2];
+      const filePath = absPath + "/" + path;
+
+      try{
+        const content = fs.readFileSync(filePath);
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+        changeResponse(res);
+      }catch(error){
+        changeResponse(`HTTP/1.1 404 Not Found\r\n\r\n`);
       }
     }
     else{
-      socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
+      changeResponse(`HTTP/1.1 404 Not Found\r\n\r\n`);
     }
 
-    // const strArr = req.split(" ");
-    // if (strArr[1] === '/'){
-
-    //   socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-    // }else{
-    //   socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
-    // }
     socket.end();
     
   })
