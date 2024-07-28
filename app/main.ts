4446da9723a8432e1d9c44b971f638b9d7abd9bc
MayurSMahajan <mayur23smahajan@gmail.com>
2024-07-28 17:54:18 +0530

    refactor: tidying up code 3

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,4 @@
+import { Socket } from "dgram";
 import * as net from "net";
 import fs from "node:fs";
 import zlib from "node:zlib";
@@ -12,58 +13,53 @@ const server = net.createServer((socket) => {
     socket.end();
   });
 
-  function changeResponse(response: string){
-    socket.write(Buffer.from(response));
-    socket.end();
-  }
-
   socket.on("data", (data) => {
     // convert byte data into 
     const req = data.toString();
     const [method, path, _] = req.split('\r\n')[0].split(' ');
-  
-    const compressionMethods = req.split("Accept-Encoding:")[1]?.split('\r\n')[0]?.split(', ');
 
     if(method === 'GET'){
+      // if no resource is requested
       if (path === '/'){
         socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
       }
+      // if the path is echo
       else if(path.startsWith('/echo/')){
         const content = path.split("/")[2];
-        let statusLine = `HTTP/1.1 200 OK\r\n`;
-        let headers = '';
-        let compressedBody;
+        const compressionMethods = req.split("Accept-Encoding:")[1]?.split('\r\n')[0]?.split(', ');
 
-        if(content && content.length){
-          headers = `Content-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
-        }
-
-        if(compressionMethods?.find((a) => a.trim() === 'gzip')){
+        if(compressionMethods?.find((a) => a.trim() === 'gzip') && content?.length){
           // compressing the body
           const buffer = Buffer.from(content, 'utf8');  
-          compressedBody = zlib.gzipSync(buffer);
-          headers = `Content-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${compressedBody.length}\r\n\r\n`;
-        }
+          const compressedBody = zlib.gzipSync(buffer);
 
-        if(headers){
-          if(compressedBody){
-            socket.write(Buffer.from(statusLine + headers));
-            socket.write(Buffer.from(compressedBody));
-          }
-          else{
-            changeResponse(statusLine + headers + '\r\n\r\n');
-          }
+          // writing the header
+          let headers = `Content-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${compressedBody.length}\r\n\r\n`;
+          
+          // writing the status line along with the header to the socket
+          socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n` + headers));
+
+          // writing the compressed data in a different socket.write() call
+          // because now body is a binary buffer after compression and thus is written separately from other string data.
+          socket.write(Buffer.from(compressedBody));
+
+        }else if(content?.length){
+          // no compression is required
+          let headers = `Content-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+          socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n` + headers + '\r\n\r\n'));
         }else{
-          changeResponse(statusLine + '\r\n')
+          // there is no content, but the request is a valid one.
+          socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
         }
       }
-      else if(path.startsWith('/user-agent/')){
+      else if(path.startsWith('/user-agent')){
         const uaInfo = req.split("User-Agent:")[1]?.split(' ')[1].trim();
         if(uaInfo){
           const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${uaInfo.length}\r\n\r\n${uaInfo}\r\n\r\n`;
-          changeResponse(res);
+          socket.write(Buffer.from(res));
         }else{
-          changeResponse(`HTTP/1.1 200 OK\r\n\r\n`);
+          // there is no user agent info
+          socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
         }
       }
       else if(path.startsWith('/files/')){
@@ -75,13 +71,13 @@ const server = net.createServer((socket) => {
         try{
           const content = fs.readFileSync(filePath);
           const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
-          changeResponse(res);
+          socket.write(Buffer.from(res));
         }catch(error){
-          changeResponse(`HTTP/1.1 404 Not Found\r\n\r\n`);
+          socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
         }
       }
       else{
-        changeResponse(`HTTP/1.1 404 Not Found\r\n\r\n`);
+        socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
       }
     }
     else if(method === 'POST' &&  path.startsWith('/files/')){
@@ -93,12 +89,12 @@ const server = net.createServer((socket) => {
         const body = req.split("\r\n\r\n")[1];
         try{
           fs.writeFileSync(filePath, body);
-          changeResponse(`HTTP/1.1 201 Created\r\n\r\n`);
+          socket.write(Buffer.from(`HTTP/1.1 201 Created\r\n\r\n`));
         }catch(error){
-          changeResponse(`HTTP/1.1 500 Internal Server Error\r\n\r\n`);
+          socket.write(Buffer.from(`HTTP/1.1 500 Internal Server Error\r\n\r\n`));
         }
     }
-
+    
     socket.end();
   })
 });
